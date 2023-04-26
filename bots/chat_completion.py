e964a59bc9577e36f243727ffcb36c5ed4d3a8d2
robindekoster <robindekoster@icloud.com>
2023-04-26 18:11:24 +0200

    Refactored code: extracted methods and added comments

--- bots/chat_completion.py
+++ bots/chat_completion.py
@@ -1,8 +1,9 @@
 import openai
 
 
-def main():
-    messages = [
+def initialize_messages() -> list:
+    """Initialize the chat messages with system and user messages."""
+    return [
         {"role": "system", "content": "You’re a kind helpful assistant, only respond with knowledge knowledge you "
                                       "know for sure, dont hallucinate information."},
         {"role": "user", "content": "Your only knowledge is about the lifespan of a dog, it depends on various "
@@ -13,18 +14,36 @@ def main():
                                     "veterinarian."}  # Replace with custom knowledge base.
     ]
 
+
+def get_user_input() -> str:
+    """Get user input from the command line."""
+    return input("User: ")
+
+
+def add_message(messages: list, role: str, content: str):
+    """Add a message to the list of chat messages."""
+    messages.append({"role": role, "content": content})
+
+
+def generate_chat_response(messages: list) -> str:
+    """Generate a chat response using the OpenAI API."""
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    return completion.choices[0].message.content
+
+
+def main():
+    messages = initialize_messages()
+
     while True:
-        content = input("User: ")
-        messages.append({"role": "user", "content": content})
+        user_message = get_user_input()
+        add_message(messages, "user", user_message)
 
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages
-        )
-
-        chat_response = completion.choices[0].message.content
+        chat_response = generate_chat_response(messages)
         print(f'ChatGPT: {chat_response}')
-        messages.append({"role": "assistant", "content": chat_response})
+        add_message(messages, "assistant", chat_response)
 
 
 if __name__ == '__main__':
